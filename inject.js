ba8ad8bccca0f9f49b5b6cdaf0e62bd997d58bac
pepperonibo <1074634342@qq.com>
2020-06-22 20:20:54 +0800

    delete logs

--- inject.js
+++ inject.js
@@ -1,9 +1,7 @@
 //插入到目标网页coursera.org的js
 //coursera双语字幕
-
 window.onload = function () {
         var myvideo = document.getElementsByTagName('video')[0];
-        var alist = document.getElementsByTagName("div");
         for (var i = 0; i < myvideo.textTracks.length; i++) {
                 ["en", "zh-CN"].indexOf(myvideo.textTracks[i].language) > -1 ? myvideo.textTracks[i].mode = "showing" : myvideo.textTracks[i].mode = "hidden";
         }
